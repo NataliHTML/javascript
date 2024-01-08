9744923c05d61b59fc669ccc0b73535bb6f99506
NataliHTML <nataly07081992@gmail.com>
2024-01-08 20:04:21 +0300

    first commit

---
+++ js/script0.js
@@ -0,0 +1,168 @@
+"use strict"
+// Урок 1
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+// 	a: 50
+// };
+// obj.a=10;
+
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+// 	var result = 50;
+// }
+
+// console.log(result);
+
+// alert(5);
+// [].push('a');
+
+// Урок2
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+// console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ', длина: ' + vehicleBodyLength);
+
+// ['grtt','rtrr','retrt'].map(a=> )
+
+// Request
+// data 
+// response
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+// let COLOR_RED = '#F00';
+
+// const _apiBase = 'https://gateway.marvel.com:443/v1/public';
+// // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
+// const _apiKey = 'apikey=ddsderwrt5667utt76dfuy67hyfu';
+
+// Урок 3
+
+// let number =4.6;
+
+// console.log(4/0);
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = `5`;
+
+// const bool = false;
+
+// console.log(something);
+// let und;
+// console.log(und);
+
+// const obj = {
+// 	name: "John",
+// 	age:25,
+// 	isMarried: false,
+// };
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []]
+
+// console.log(arr[1]);
+
+// Урок 4
+
+// const arr = ['a', 'b', 'c'];
+// const arrObj ={
+// 	a: 'a',
+// 	'1': 'b',
+// 	2: 'c',
+	// abc:{
+	// 	de: [{},{}],
+	// 	def:{
+
+	// 	}
+	// }
+// };
+
+// const b ='b'
+
+// arrObj.b='1234'
+// arrObj['b']='1234'
+// arrObj[b]=[]
+// arrObj[b]={}
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// const obj = {a: 1, b: 2};
+// const obj = {
+// 	Anna: 500,
+// 	'Alice': 800
+// };
+
+// Урок 5
+
+// alert('Hello')
+// const result = confirm("Are you here?")
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18")
+// console.log(answer + 5);
+
+// const answers = []
+
+// answers[0] = prompt('Как ваше имя?', '')
+// answers[1] = prompt('Как ваше фамилия?', '')
+// answers[2] = prompt('Сколько вам лет?', '')
+
+// document.write(answers)
+// console.log(typeof(answers))
+// console.log(typeof(null))
+
+// Урок 6
+
+// const category = 'toys';
+
+// // console.log('https://someurl.com/' + category + '/' + '4');
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`)
+
+// Урок 7
+
+// console.log(4 + +"5");
+
+// let  incr = 10;
+// 		 decr = 10;
+
+// 		// ++incr;
+// 		// --decr;
+
+// 		console.log(++incr);
+// 		console.log(--decr);
+
+// 		console.log(5%2);
+
+// 		console.log(2*4 == '8');
+// 		console.log(2*4 === '8');
+// 		console.log(2+2*2 === 8);
+// 		console.log(2 + 2*2 != '6');
+// 		console.log(2 + 2*2 !== '6');
+
+// 		// && и 
+// 		// || или 
+// 		const isChecked = false;
+// 					isClose = false;
+
+// 		// console.log(isChecked && isClose);
+// 		console.log(isChecked || !isClose);
+
+// Урок 8
